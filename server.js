f082b097d8c75b94ea3badbdac1152500e1bdb57
Francesco <fra.boasso@gmail.com>
2023-11-18 17:02:08 +0100

    🔨  Remade the whole file

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
-const { default: axios } = require("axios");
-const express = require("express");
-const app = express();
-const port = 3000;
+import { default as axios } from "axios";
+import { default as express, response } from "express";
+import cors from 'cors';
 
-app.get("/test", async (req, res) => {
-    try {
-        const response = await axios.get('https://www.dnd5eapi.co/api/')
-        res.json(response.data)
-    }
-    catch (err) {
-        console.log(err)
-    }
+const app = express();
+app.use(cors());
+
+// Level 0 API
+app.get("/main", async (req, res) => {
+    const response = await axios.get('https://www.dnd5eapi.co/api/')
+    res.json(response.data)
 })
 
-// App listens to incoming requests here
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+// First Level API
+app.get("/main/:category/", async (req, res) => {
+    res.setTimeout(1000, () => {
+        const category = req.params.category;
+        axios.get(`https://www.dnd5eapi.co/api/${category}`)
+            .then(response => res.json(response.data))
+            .catch(() => res.json(response.data))
+        return
+    })
+})
+
+// Second Level API
+app.get("/main/:category/:subcategory", async (req, res) => {
+    res.setTimeout(1000, () => {
+        const category = req.params.category;
+        const subcategory = req.params.subcategory;
+        if (subcategory) {
+            axios.get(`https://www.dnd5eapi.co/api/${category}/${subcategory}`)
+                .then(response => {
+                    res.json(response.data)
+                }).catch(() => res.json(response.data))
+        }
+        else
+            axios.get(`https://www.dnd5eapi.co/api/${category}`)
+        return res.json(response.data)
+    })
+})
+
+app.listen(3000);
